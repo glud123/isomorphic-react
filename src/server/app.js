235d92abec1d353349e7982afa572bc89cdcfdab
gaokung <gaokung@gaokungdeMacBook-Pro.local>
2020-04-23 14:06:04 +0800

    *koa重定向

--- src/server/app.js
+++ src/server/app.js
@@ -1,14 +1,15 @@
 const Koa = require("koa");
+const favicon = require("koa-favicon");
 
 const app = new Koa();
+// favicon
+app.use(favicon(__dirname + "/favicon.ico"));
 
 // logger
 app.use(async (ctx, next) => {
   await next();
   const rt = ctx.response.get("X-Response-Time");
-  if (ctx.url != "/favicon.ico") {
-    console.log(`${ctx.method} ${ctx.url} - ${rt}`);
-  }
+  console.log(`${ctx.method} ${ctx.url} - ${rt}`);
 });
 
 // X-Response-Time
@@ -19,9 +20,30 @@ app.use(async (ctx, next) => {
   ctx.set("X-Response-Time", `${ms}ms`);
 });
 
+// 重定向
+app.use(async (ctx, next) => {
+  if (ctx.url === "/user") {
+    ctx.redirect("/login");
+  }else{
+    await next();
+  }
+});
+
 // resopnse
 app.use(async (ctx) => {
-  ctx.body = "hello world KOA";
+  switch (ctx.url) {
+    case "/":
+      ctx.body = "hello world KOA";
+      break;
+    case "/user":
+      ctx.body = "User";
+      break;
+    case "/login":
+      ctx.body = "Login";
+      break;
+    default:
+      break;
+  }
 });
 
 app.listen(3000);
