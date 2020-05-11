a26061a491e30b6f3caaec0df3c1332b0058e722
glud123 <glud123@163.com>
2020-05-11 19:22:12 +0800

    *适配webpack配置重构

--- scripts/util.js
+++ scripts/util.js
@@ -1,9 +1,11 @@
 const chalk = require("chalk");
 const path = require("path");
+const open = require("open");
+const spawn = require("cross-spawn");
 
 // 路径转换
 const resolvePath = (pathStr) => {
-  return path.resolve(__dirname, pathStr);
+  return path.resolve(__dirname, `../${pathStr}`);
 };
 
 // 控制台彩色输出
@@ -11,7 +13,40 @@ const logger = (str, type = "yellow") => {
   console.log(chalk[type](str));
 };
 
+// 打开浏览器
+async function openBrowser(url) {
+  await open(url, { app: ["google chrome", "--incognito"] });
+}
+// 端口释放
+const freePort = (port, appPath) => {
+  const lsofWatcher = spawn("lsof", ["-i", `:${port}`]);
+  lsofWatcher.on("close", (code) => {
+    if (typeof code === "number" && appPath) {
+      require(appPath);
+    }
+  });
+  lsofWatcher.stdout.on("data", (data) => {
+    let dataStr = data.toString();
+    logger(`💣 FreePort ${port}`, "red");
+    dataStr.split("\n").map((line) => {
+      let p = line.trim().split(/\s+/);
+      let address = p[1];
+      if (address && address != "PID") {
+        const killWatcher = spawn("kill", ["-9", address]);
+        killWatcher.on("close", (code) => {
+          logger(`💣 FreePort ${port}`, "red");
+          if (typeof code === "number" && appPath) {
+            require(appPath);
+          }
+        });
+      }
+    });
+  });
+};
+
 module.exports = {
   resolvePath: resolvePath,
   logger: logger,
+  openBrowser: openBrowser,
+  freePort: freePort,
 };
--- src/server/util/handleAssets.js
+++ src/server/util/handleAssets.js
@@ -12,7 +12,6 @@ module.exports = () => {
   if (__IS_PROD__) {
     // 生产环境
     const assetsMap = require("@dist/static/manifest.json");
-    console.log(assetsMap);
     jsFiles.forEach((item) => {
       if (assetsMap[item]) {
         assets.js.push(
@@ -30,10 +29,7 @@ module.exports = () => {
   } else {
     // 开发环境
     assets.js.push(
-      `<script type="text/javascript" src="${devHost}/libs.js"></script>`
-    );
-    assets.js.push(
-      `<script type="text/javascript" src="${devHost}/main.js"></script>`
+      `<script type="text/javascript" src="${devHost}/js/main.js"></script>`
     );
   }
   return assets;
